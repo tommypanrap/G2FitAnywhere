59def4afc441f1706ec42f77d3073ffd0e68a644
Tommy <liweidesigner@gmail.com>
2024-02-14 11:21:49 +0800

    first commit

---
+++ src/main/java/com/fitanywhere/social/model/SocialPostDAOImpl.java
@@ -0,0 +1,60 @@
+package com.fitanywhere.social.model;
+
+import java.util.*;
+import java.sql.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.fitanywhere.social.model.SocialPostVO;
+import com.fitanywhere.util.HibernateUtil;
+
+public class SocialPostDAOImpl implements SocialPostDAO{
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/fitanywhere");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private SessionFactory factory;
+	
+	public SocialPostDAOImpl() {
+		factory = HibernateUtil.getSessionFactory();
+	}
+	
+	private Session getSession() {
+		return factory.getCurrentSession();
+	}
+	
+	@Override
+	public void insert(SocialPostVO entity) {
+		// 回傳給 service 剛新增成功的自增主鍵值
+		getSession().save(entity);
+	}
+	
+	@Override
+	public void update(SocialPostVO entity) {
+		getSession().update(entity);
+	}
+	
+	@Override
+	public SocialPostVO findByPrimaryKey(Integer spid) {
+		return getSession().get(SocialPostVO.class, spid);
+	}
+	
+	@Override
+	public List<SocialPostVO> getAll() {
+		return getSession().createQuery("from social_post", SocialPostVO.class).list();
+	}
+	
+}
