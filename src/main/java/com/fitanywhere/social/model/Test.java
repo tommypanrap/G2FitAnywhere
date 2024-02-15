ca4e283d924a458f9a54672927e4ccacba36ea98
Tommy <liweidesigner@gmail.com>
2024-02-15 19:59:50 +0800

    test

--- src/main/java/com/fitanywhere/social/model/Test.java
+++ src/main/java/com/fitanywhere/social/model/Test.java
@@ -10,27 +10,28 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class Test {
 
 	public static void main(String[] args) {
-		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure()
-				.build();
+//		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+//				.configure()
+//				.build();
+//		
+//		SessionFactory factory = new MetadataSources(registry)
+//				.buildMetadata()
+//				.buildSessionFactory();
+//		
+//		Session session = factory.openSession();
+//		
+//		Transaction tx = session.beginTransaction();
+//		
+//		
+//		SocialPostVO socialPostVO1 = session.get(SocialPostVO.class, 1);
+//		System.out.println(socialPostVO1);
+//		
+//		
+//		tx.commit();
+//		session.close();
+//		factory.close();
 		
-		SessionFactory factory = new MetadataSources(registry)
-				.buildMetadata()
-				.buildSessionFactory();
-		
-		Session session = factory.openSession();
-		
-		Transaction tx = session.beginTransaction();
-		
-		
-		SocialPostVO socialPostVO1 = session.get(SocialPostVO.class, 1);
-		System.out.println(socialPostVO1);
-		
-		
-		tx.commit();
-		session.close();
-		factory.close();
-
+		System.out.println("Hi");
 	}
 
 }
