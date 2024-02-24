59fc3f94c90a773c2ac01f452f8e637a8b45fa89
Tommy <liweidesigner@gmail.com>
2024-02-24 11:28:36 +0800

    test commit push pr

--- src/main/java/com/fitanywhere/socialpost/model/SocialPostService.java
+++ src/main/java/com/fitanywhere/socialpost/model/SocialPostService.java
@@ -24,11 +24,6 @@ public class SocialPostService implements SocialPostDAO{
 		dao.update(socialPostVO);
 	}
 
-	@Override
-	public SocialPostVO findByPrimaryKey(Integer spid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public List<SocialPostVO> getAll() {
@@ -36,6 +31,11 @@ public class SocialPostService implements SocialPostDAO{
 		return null;
 	}
 	
+	@Override
+	public SocialPostVO findByPrimaryKey(Integer spid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 
